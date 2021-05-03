9d8192edb258ec644d323ef203370bb92d06c320
d00d1e <lee.julianay@gmail.com>
2021-05-03 13:21:20 -0700

    starter files

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <div className="title">
+      <h1>Pictogram</h1>
+      <h2>Photo Memories</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    </div>
+  );
+}
